7180b0ad3a9bc3d4468301932fcd29a14e083505
Anderson <gomezsanchezz830@gmail.com>
2026-05-15 00:05:06 -0600

    primer bot

---
+++ bot.py
@@ -0,0 +1,26 @@
+from telegram import *
+from telegram.ext import *
+
+TOKEN = "8906807130:AAEfNnBvsRAcMwQfdgwlZ1dSRt6nofsfD2M"
+
+async def start(update, context):
+    buttons = [
+        [InlineKeyboardButton("➕ Cargar saldo", callback_data="saldo"),
+         InlineKeyboardButton("💰 Mi saldo", callback_data="misaldo")],
+
+        [InlineKeyboardButton("🛒 Comprar cuentas", callback_data="comprar")],
+
+        [InlineKeyboardButton("🆘 Soporte", callback_data="soporte")]
+    ]
+
+    await update.message.reply_text(
+        "👤 BIENVENIDO AL PANEL USUARIO",
+        reply_markup=InlineKeyboardMarkup(buttons)
+    )
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+print("BOT ONLINE")
+app.run_polling()
